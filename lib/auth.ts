1d188c2c81d0022be913b5d2a9d3ea6c9c12c5c0
Elvin Bashirli <ebashirli@gmail.com>
2026-04-23 15:12:07 +0400

    feat: integrate better-auth for authentication and session management
    - Added better-auth and its MongoDB adapter as dependencies.
    - Implemented authentication routes using better-auth in `app/api/auth/[...all]/route.ts`.
    - Created an auth client in `lib/auth-client.ts` for session management.
    - Configured authentication with MongoDB in `lib/auth.ts`.
    - Developed sign-in functionality with social providers in `lib/sign-in.ts`.
    - Implemented session validation middleware in `proxy.ts` for protected routes.

---
+++ lib/auth.ts
@@ -0,0 +1,21 @@
+import { betterAuth } from "better-auth";
+import { MongoClient } from "mongodb";
+import { mongodbAdapter } from "better-auth/adapters/mongodb";
+
+const uri =
+  process.env.MONGODB_URI?.replace(
+    "<password>",
+    process.env.MONGODB_PASSWORD ?? "",
+  ) ?? "";
+
+const client = new MongoClient(uri);
+const db = client.db();
+export const auth = betterAuth({
+  database: mongodbAdapter(db, { client }),
+  socialProviders: {
+    google: {
+      clientId: process.env.AUTH_GOOGLE_ID ?? "",
+      clientKey: process.env.AUTH_GOOGLE_SECRET,
+    },
+  },
+});
