f3f635931eb420ac602913dfebedf9701779df86
Elvin Bashirli <ebashirli@gmail.com>
2026-05-01 19:54:47 +0400

    feat: add Company and Employee models, implement check-in functionality, and update dependencies

--- app/api/auth/[...all]/checkins/route.tsx
+++ app/api/auth/[...all]/checkins/route.tsx
@@ -1,5 +1,5 @@
 import { auth } from "@/lib/auth";
-import { Time } from "@/lib/models/Time";
+import prisma from "@/lib/prisma";
 import { headers } from "next/headers";
 import { redirect } from "next/navigation";
 type Prop = {
@@ -8,18 +8,20 @@ type Prop = {
 
 const Page = async ({ params }: Prop) => {
   const session = await auth.api.getSession({ headers: await headers() });
-  if (!session || !session.user) redirect("/sign-in");
+  if (!session || !session.user) return redirect("/sign-in");
 
   const { id } = await params;
 
-  const newTime = new Time({
-    employeeId: id,
-    postId: session.user.email,
-    time: new Date(),
+  const newEntry = await prisma.checkin?.create({
+    data: { employeeId: id, postId: session.user.email },
+    include: { employee: true },
   });
-  await newTime.save();
 
-  return <div>Employee Id: {id}</div>;
+  console.log({ newEntry });
+
+  if (!newEntry) return null;
+
+  return <div>Employee Id: {newEntry.employeeId}</div>;
 };
 
 export default Page;
