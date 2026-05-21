5caccf015e3eefd583a7bbd651c0cb170c7667df
Elvin Bashirli <ebashirli@gmail.com>
2026-05-21 11:18:38 +0400

    add motion elements

--- components/EmployeeCard.tsx
+++ components/EmployeeCard.tsx
@@ -16,6 +16,7 @@ import { DeleteDialog } from "./DeleteDialog";
 import { Button } from "./ui/button";
 import { deleteEmployee } from "@/actions/deleteEmployee";
 import { QRGenerator } from "./QRGenerator";
+import TiltedCard from "./TiledCard/TiltedCard";
 
 type Employee = {
   id: string;
@@ -46,53 +47,72 @@ export const EmployeeCard = ({
   };
 
   return (
-    <Card
-      className={cn(
-        "mx-auto w-full max-w-sm rounded-xl bg-muted/50 border-2 py-3",
-        "transition-all hover:bg-accent hover:shadow-md cursor-pointer ",
-      )}
-    >
-      <Link href={`/employees/${employee.id}`}>
-        <CardHeader>
-          <div className="flex items-center gap-3 min-h-14 ">
-            <Avatar className="h-8 w-8 rounded-lg">
-              {employee && (
-                <AvatarImage
-                  src={employee.image}
-                  alt={employee.fullName ?? ""}
+    <TiltedCard
+      // imageSrc="https://i.scdn.co/image/ab67616d0000b273d9985092cd88bffd97653b58"
+      // altText="Kendrick Lamar - GNX Album Cover"
+      // captionText="Kendrick Lamar - GNX"
+      // containerHeight="300px"
+      // containerWidth="300px"
+      // imageHeight="300px"
+      // imageWidth="300px"
+      rotateAmplitude={15}
+      scaleOnHover={1.05}
+      showMobileWarning={false}
+      showTooltip
+      displayOverlayContent
+      overlayContent={
+        <Card
+          className={cn(
+            "mx-auto w-full max-w-sm rounded-xl bg-muted/50 border-2 py-3",
+            "transition-all hover:bg-accent hover:shadow-md cursor-pointer ",
+          )}
+        >
+          <Link href={`/employees/${employee.id}`}>
+            <CardHeader>
+              <div className="flex items-center gap-3 min-h-14 ">
+                <Avatar className="h-8 w-8 rounded-lg">
+                  {employee && (
+                    <AvatarImage
+                      src={employee.image}
+                      alt={employee.fullName ?? ""}
+                    />
+                  )}
+                  <AvatarFallback className="rounded-lg">
+                    {employee.fullName?.slice(0, 2).toLocaleUpperCase()}
+                  </AvatarFallback>
+                </Avatar>
+                <CardTitle>{employee.fullName}</CardTitle>
+              </div>
+              <CardDescription>{employee.position.name}</CardDescription>
+            </CardHeader>
+            <CardContent>
+              <p>{employee.company.name}</p>
+              <p>{employee.department?.name}</p>
+            </CardContent>
+            <Separator className="my-3" />
+            <CardFooter className="flex justify-end">
+              <div
+                className="flex items-center gap-3"
+                onClick={handlePropogation}
+              >
+                <Button
+                  asChild
+                  variant={"outline"}
+                  className="min-w-10 cursor-pointer "
+                >
+                  <Pencil />
+                </Button>
+                <QRGenerator value={employee.cardId} />
+                <DeleteDialog
+                  deleteAction={deleteEmployee}
+                  id={employee.id}
+                  setData={setEmployees}
                 />
-              )}
-              <AvatarFallback className="rounded-lg">
-                {employee.fullName?.slice(0, 2).toLocaleUpperCase()}
-              </AvatarFallback>
-            </Avatar>
-            <CardTitle>{employee.fullName}</CardTitle>
-          </div>
-          <CardDescription>{employee.position.name}</CardDescription>
-        </CardHeader>
-        <CardContent>
-          <p>{employee.company.name}</p>
-          <p>{employee.department?.name}</p>
-        </CardContent>
-        <Separator className="my-3" />
-        <CardFooter className="flex justify-end">
-          <div className="flex items-center gap-3" onClick={handlePropogation}>
-            <Button
-              asChild
-              variant={"outline"}
-              className="min-w-10 cursor-pointer "
-            >
-              <Pencil />
-            </Button>
-            <QRGenerator value={employee.cardId} />
-            <DeleteDialog
-              deleteAction={deleteEmployee}
-              id={employee.id}
-              setData={setEmployees}
-            />
-          </div>
-        </CardFooter>
-      </Link>
-    </Card>
+              </div>
+            </CardFooter>
+          </Link>
+        </Card>
+      }
+    />
   );
 };
