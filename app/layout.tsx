772dc52fd61d9ac9b7c756021f2920186db5ac6a
Elvin Bashirli <ebashirli@gmail.com>
2026-06-23 22:02:23 +0400

    change file location fo register sw

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import { Geist, Geist_Mono, Inter } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
 import Providers from "@/components/Providers";
-import { RegisterSW } from "./register-sw";
+import { RegisterSW } from "@/components/register-sw";
 
 const inter = Inter({ subsets: ["latin"], variable: "--font-sans" });
 
