d15ff64a209ae003a153ae7fc850c630d398d5b9
Elvin Bashirli <ebashirli@gmail.com>
2026-06-25 10:12:20 +0400

    fix host-cert connection

--- server.ts
+++ server.ts
@@ -1,17 +1,19 @@
-import https from 'https';
-import next from 'next';
-import fs from 'fs';
+import "dotenv/config";
+import https from "https";
+import next from "next";
+import fs from "fs";
 
-const dev = process.env.NODE_ENV !== 'production';
-const hostname = '0.0.0.0';
+const dev = process.env.NODE_ENV !== "production";
+const hostname = "0.0.0.0";
 const port = 3000;
 
 const app = next({ dev });
 const handle = app.getRequestHandler();
+const host = process.env.BETTER_AUTH_URL?.split(":").at(1)?.replace("//", "");
 
 const httpsOptions = {
-  key: fs.readFileSync('./10.10.8.253+2-key.pem'),
-  cert: fs.readFileSync('./10.10.8.253+2.pem'),
+  key: fs.readFileSync(`./${host}+2-key.pem`),
+  cert: fs.readFileSync(`./${host}+2.pem`),
 };
 
 app.prepare().then(() => {
@@ -23,6 +25,6 @@ app.prepare().then(() => {
 
   server.listen(port, hostname, () => {
     console.log(`✅ Server ready`);
-    console.log(`https://10.10.8.253:${port}`);
+    console.log(`https://${host}:${port}`);
   });
 });
